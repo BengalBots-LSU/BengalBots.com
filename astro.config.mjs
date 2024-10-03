af143e2ddb2fd3941bb054722467f9a046aa7019
AZapie <azaier03@gmail.com>
2024-10-03 18:30:05 -0500

    Added about us section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from "@astrojs/react";
 import CompressionPlugin from "vite-plugin-compression";
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from "@astrojs/tailwind";
+
 export const siteUrl = "https://bengalbots.com";
 
 const date = new Date().toISOString();
@@ -10,35 +12,32 @@ const date = new Date().toISOString();
 export default defineConfig({
     site: 'https://bengalbots.com',
 
-    integrations: [
-        react(),
-        sitemap({
-            serialize(item) {
-                // Default values for pages
-                item.priority = siteUrl + "/" === item.url ? 1.0 : 0.9;
-                item.changefreq = "weekly";
-                item.lastmod = date;
+    integrations: [react(), sitemap({
+        serialize(item) {
+            // Default values for pages
+            item.priority = siteUrl + "/" === item.url ? 1.0 : 0.9;
+            item.changefreq = "weekly";
+            item.lastmod = date;
 
-                // if you want to exclude a page from the sitemap, do it here
-                // if (/exclude-from-sitemap/.test(item.url)) {
-                //     return undefined;
-                // }
+            // if you want to exclude a page from the sitemap, do it here
+            // if (/exclude-from-sitemap/.test(item.url)) {
+            //     return undefined;
+            // }
 
-                // if any page needs a different priority, changefreq, or lastmod, uncomment the following lines and adjust as needed
-                // if (/test-sitemap/.test(item.url)) {
-                //     item.changefreq = "daily";
-                //     item.lastmod = date;
-                //     item.priority = 0.9;
-                // }
+            // if any page needs a different priority, changefreq, or lastmod, uncomment the following lines and adjust as needed
+            // if (/test-sitemap/.test(item.url)) {
+            //     item.changefreq = "daily";
+            //     item.lastmod = date;
+            //     item.priority = 0.9;
+            // }
 
-                // if you want to change priority of all subpages like "/posts/*", you can use:
-                // if (/\/posts\//.test(item.url)) {
-                //     item.priority = 0.7;
-                // }
-                return item;
-            },
-        }),
-    ],
+            // if you want to change priority of all subpages like "/posts/*", you can use:
+            // if (/\/posts\//.test(item.url)) {
+            //     item.priority = 0.7;
+            // }
+            return item;
+        },
+    }), tailwind()],
     renderers: ["@astrojs/renderer-react"],
     prerender: true,
     vite: {
